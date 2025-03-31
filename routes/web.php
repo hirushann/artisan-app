36be06e2707b0e1ad5f06c6f0573b0691c19caa0
Hasith Udayanga <uhasith5@gmail.com>
2025-03-31 10:56:09 +0530

    set dashboard as default page

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
 });
